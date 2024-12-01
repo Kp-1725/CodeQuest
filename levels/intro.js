9a2a253c581e64e5cf763bb9b5c4bbf66cbd3977
Kp <kpnihaal1725@gmail.com>
2024-12-01 05:45:00 +0530

    Update intro.js

--- levels/intro.js
+++ levels/intro.js
@@ -1,14 +1,19 @@
 function playIntro(display, map, i) {
-	if (i < 0) {
-        display._intro = true;
+    if (typeof i === 'undefined') { 
+        i = map.getHeight(); 
+    }
+
+    if (i < 0) {
+        display._intro = true; // Mark intro as complete
     } else {
-        if (typeof i === 'undefined') { i = map.getHeight(); }
+        display.clear();
         display.drawText(0, i - 2, "%c{#0f0}> initialize");
         display.drawText(15, i + 3, "Code Quest");
         display.drawText(3, i + 12, "a game by Curly Braces");
         display.drawText(10, i + 22, "Press any key to begin ...");
+
         setTimeout(function () {
-            display.playIntro(map, i - 1);
+            playIntro(display, map, i - 1); // Correct recursive call
         }, 100);
     }
 }
